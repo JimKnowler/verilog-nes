d6194aee3c262e493c199cd1309e3bbbb7dcf0f4
Jim Knowler <jim.knowler2@gmail.com>
2021-01-01 17:07:01 +0000

    TCU: remove unimplemented SYNC

--- nes/cpu6502/TCU.v
+++ nes/cpu6502/TCU.v
@@ -1,19 +1,20 @@
 // Timing Control Unit
 
+/// @todo SYNC output 
+/// @note The hanson diagram shows SYNC being high during T1
+///       but we know SYNC should be high while fetching opcode
+///       and that opcode is fetched during T0 
+///       ... does T1 == T0 somehow?
+
 module TCU(
     input i_clk,
     input i_reset_n,
     input [2:0] i_tcu_next,
-    output [2:0] o_tcu,
-    output o_sync
+    output [2:0] o_tcu
 );
 
 reg [2:0] r_tcu;
 reg r_has_started;
-
-/* verilator lint_off UNDRIVEN */
-reg r_sync;
-/* verilator lint_on UNDRIVEN */
 
 always @(negedge i_clk or negedge i_reset_n)
 begin
@@ -32,6 +33,5 @@ begin
 end
 
 assign o_tcu = r_tcu;
-assign o_sync = r_sync;
 
 endmodule
