c2b43231d8f7fd937285b14b91769f34a53ec6b7
Jim Knowler <jim.knowler2@gmail.com>
2021-01-16 16:10:45 +0000

    ProcessorStatus: implement i_db0_c, i_db1_z, i_db2_i, i_db3_d, i_db4_b, and i_db6_v

--- nes/cpu6502/ProcessorStatus.test.cpp
+++ nes/cpu6502/ProcessorStatus.test.cpp
@@ -73,7 +73,7 @@ TEST_F(ProcessorStatus, ShouldNotLoadNFromPositiveDB7) {
     EXPECT_THAT(testBench.trace, MatchesTrace(expected));
 }
 
-TEST_F(ProcessorStatus, ShouldLoadZFromZeroDB) {
+TEST_F(ProcessorStatus, ShouldLoadZFromDBZ) {
     auto& core = testBench.core();
     
     core.i_db = 0;
@@ -92,7 +92,7 @@ TEST_F(ProcessorStatus, ShouldLoadZFromZeroDB) {
     EXPECT_THAT(testBench.trace, MatchesTrace(expected));
 }
 
-TEST_F(ProcessorStatus, ShouldNotLoadZFromNonZeroDB7) {
+TEST_F(ProcessorStatus, ShouldClearZFromDBZ) {
     auto& core = testBench.core();
     
     core.i_db = 0xff;
@@ -107,6 +107,47 @@ TEST_F(ProcessorStatus, ShouldNotLoadZFromNonZeroDB7) {
 
     Trace expected = TraceBuilder()
         .port(o_p).signal({0}).repeat(6);
+
+    EXPECT_THAT(testBench.trace, MatchesTrace(expected));
+}
+
+TEST_F(ProcessorStatus, ShouldSetZFromDB1) {
+    auto& core = testBench.core();
+    
+    core.i_db = Z;
+    core.i_db1_z = 0;
+    testBench.tick();
+
+    core.i_db1_z = 1;
+    testBench.tick();
+
+    core.i_db1_z = 0;
+    testBench.tick();
+
+    Trace expected = TraceBuilder()
+        .port(o_p).signal({0,Z,Z}).repeatEachStep(2);
+
+    EXPECT_THAT(testBench.trace, MatchesTrace(expected));
+}
+
+TEST_F(ProcessorStatus, ShouldClearZFromDB1) {
+    auto& core = testBench.core();
+    
+    core.i_db = Z;
+    core.i_db1_z = 0;
+    testBench.tick();
+
+    core.i_db1_z = 1;
+    testBench.tick();
+
+    core.i_db = 0;
+    testBench.tick();
+
+    core.i_db1_z = 0;
+    testBench.tick();
+
+    Trace expected = TraceBuilder()
+        .port(o_p).signal({0,Z,0,0}).repeatEachStep(2);
 
     EXPECT_THAT(testBench.trace, MatchesTrace(expected));
 }
@@ -191,6 +232,48 @@ TEST_F(ProcessorStatus, ShouldClearCFromIR5) {
     EXPECT_THAT(testBench.trace, MatchesTrace(expected));
 }
 
+TEST_F(ProcessorStatus, ShouldSetCFromDB0) {
+    auto& core = testBench.core();
+    
+    core.i_db = C;
+    core.i_db0_c = 0;
+    testBench.tick();
+
+    core.i_db0_c = 1;
+    testBench.tick();
+
+    core.i_db0_c = 0;
+    testBench.tick();
+
+    Trace expected = TraceBuilder()
+        .port(o_p).signal({0,C,C}).repeatEachStep(2);
+
+    EXPECT_THAT(testBench.trace, MatchesTrace(expected));
+}
+
+
+TEST_F(ProcessorStatus, ShouldClearCFromDB0) {
+    auto& core = testBench.core();
+    
+    core.i_db = C;
+    core.i_db0_c = 0;
+    testBench.tick();
+
+    core.i_db0_c = 1;
+    testBench.tick();
+
+    core.i_db = 0;
+    testBench.tick();
+
+    core.i_ir5_c = 0;
+    testBench.tick();
+
+    Trace expected = TraceBuilder()
+        .port(o_p).signal({0,C,0,0}).repeatEachStep(2);
+
+    EXPECT_THAT(testBench.trace, MatchesTrace(expected));
+}
+
 TEST_F(ProcessorStatus, ShouldSetIFromIR5) {
     auto& core = testBench.core();
     
@@ -210,7 +293,6 @@ TEST_F(ProcessorStatus, ShouldSetIFromIR5) {
     EXPECT_THAT(testBench.trace, MatchesTrace(expected));
 }
 
-
 TEST_F(ProcessorStatus, ShouldClearIFromIR5) {
     auto& core = testBench.core();
     
@@ -224,11 +306,175 @@ TEST_F(ProcessorStatus, ShouldClearIFromIR5) {
     core.i_ir5 = 0;
     testBench.tick();
 
-    core.i_ir5_c = 0;
+    core.i_ir5_i = 0;
     testBench.tick();
 
     Trace expected = TraceBuilder()
         .port(o_p).signal({0,I,0,0}).repeatEachStep(2);
+
+    EXPECT_THAT(testBench.trace, MatchesTrace(expected));
+}
+
+TEST_F(ProcessorStatus, ShouldSetIFromDB2) {
+    auto& core = testBench.core();
+    
+    core.i_db = I;
+    core.i_db2_i = 0;
+    testBench.tick();
+
+    core.i_db2_i = 1;
+    testBench.tick();
+
+    core.i_db2_i = 0;
+    testBench.tick();
+
+    Trace expected = TraceBuilder()
+        .port(o_p).signal({0,I,I}).repeatEachStep(2);
+
+    EXPECT_THAT(testBench.trace, MatchesTrace(expected));
+}
+
+TEST_F(ProcessorStatus, ShouldClearIFromDB2) {
+    auto& core = testBench.core();
+    
+    core.i_db = I;
+    core.i_db2_i = 0;
+    testBench.tick();
+
+    core.i_db2_i = 1;
+    testBench.tick();
+
+    core.i_db = 0;
+    testBench.tick();
+
+    core.i_db2_i = 0;
+    testBench.tick();
+
+    Trace expected = TraceBuilder()
+        .port(o_p).signal({0,I,0,0}).repeatEachStep(2);
+
+    EXPECT_THAT(testBench.trace, MatchesTrace(expected));
+}
+
+TEST_F(ProcessorStatus, ShouldSetDFromDB3) {
+    auto& core = testBench.core();
+    
+    core.i_db = D;
+    core.i_db3_d = 0;
+    testBench.tick();
+
+    core.i_db3_d = 1;
+    testBench.tick();
+
+    core.i_db3_d = 0;
+    testBench.tick();
+
+    Trace expected = TraceBuilder()
+        .port(o_p).signal({0,D,D}).repeatEachStep(2);
+
+    EXPECT_THAT(testBench.trace, MatchesTrace(expected));
+}
+
+TEST_F(ProcessorStatus, ShouldClearDFromDB3) {
+    auto& core = testBench.core();
+    
+    core.i_db = D;
+    core.i_db3_d = 0;
+    testBench.tick();
+
+    core.i_db3_d = 1;
+    testBench.tick();
+
+    core.i_db = 0;
+    testBench.tick();
+
+    core.i_db3_d = 0;
+    testBench.tick();
+
+    Trace expected = TraceBuilder()
+        .port(o_p).signal({0,D,0,0}).repeatEachStep(2);
+
+    EXPECT_THAT(testBench.trace, MatchesTrace(expected));
+}
+
+TEST_F(ProcessorStatus, ShouldSetBFromDB4) {
+    auto& core = testBench.core();
+    
+    core.i_db = B;
+    core.i_db4_b = 0;
+    testBench.tick();
+
+    core.i_db4_b = 1;
+    testBench.tick();
+
+    core.i_db4_b = 0;
+    testBench.tick();
+
+    Trace expected = TraceBuilder()
+        .port(o_p).signal({0,B,B}).repeatEachStep(2);
+
+    EXPECT_THAT(testBench.trace, MatchesTrace(expected));
+}
+
+TEST_F(ProcessorStatus, ShouldClearBFromDB4) {
+    auto& core = testBench.core();
+    
+    core.i_db = B;
+    core.i_db4_b = 0;
+    testBench.tick();
+
+    core.i_db4_b = 1;
+    testBench.tick();
+
+    core.i_db = 0;
+    testBench.tick();
+
+    core.i_db4_b = 0;
+    testBench.tick();
+
+    Trace expected = TraceBuilder()
+        .port(o_p).signal({0,B,0,0}).repeatEachStep(2);
+
+    EXPECT_THAT(testBench.trace, MatchesTrace(expected));
+}
+
+TEST_F(ProcessorStatus, ShouldSetVFromDB6) {
+    auto& core = testBench.core();
+    
+    core.i_db = V;
+    core.i_db6_v = 0;
+    testBench.tick();
+
+    core.i_db6_v = 1;
+    testBench.tick();
+
+    core.i_db6_v = 0;
+    testBench.tick();
+
+    Trace expected = TraceBuilder()
+        .port(o_p).signal({0,V,V}).repeatEachStep(2);
+
+    EXPECT_THAT(testBench.trace, MatchesTrace(expected));
+}
+
+TEST_F(ProcessorStatus, ShouldClearVFromDB6) {
+    auto& core = testBench.core();
+    
+    core.i_db = V;
+    core.i_db6_v = 0;
+    testBench.tick();
+
+    core.i_db6_v = 1;
+    testBench.tick();
+
+    core.i_db = 0;
+    testBench.tick();
+
+    core.i_db6_v = 0;
+    testBench.tick();
+
+    Trace expected = TraceBuilder()
+        .port(o_p).signal({0,V,0,0}).repeatEachStep(2);
 
     EXPECT_THAT(testBench.trace, MatchesTrace(expected));
 }
--- nes/cpu6502/ProcessorStatus.v
+++ nes/cpu6502/ProcessorStatus.v
@@ -3,7 +3,6 @@
 // store C, Z, I, D, B, V and N flags
 
 module ProcessorStatus(
-    /* verilator lint_off UNUSED */
     input i_clk,
     input i_reset_n,
 
@@ -17,38 +16,38 @@ module ProcessorStatus(
     input i_acr,                        // ALU Carry
     /*
     input i_avr,                        // ALU overflow
+    */
 
     // CARRY control signals
     input i_db0_c,
-    */
     input i_ir5_c,
     input i_acr_c,
 
     // ZERO control signals
-    /*
     input i_db1_z,
-    */
     input i_dbz_z,
-    /*
 
     // INTERRUPT control signals
     input i_db2_i,
-    */
     input i_ir5_i,
-    /*
 
     // DECIMAL control signals
     input i_db3_d,
+    /*
     input i_ir5_d,
+    */
+
+    // BREAK control signals
+    input i_db4_b,
 
     // OVERFLOW control signals
     input i_db6_v,
+    /*
     input i_avr_v,
     input i_1_v,
     */
 
     input i_db7_n
-    /* verilator lint_on UNUSED */
 );
 
 localparam C = 0;       // Carry Flag
@@ -64,7 +63,10 @@ assign w_dbz = ~(|i_db);
 
 reg r_c;
 reg r_z;
+reg r_d;
 reg r_i;
+reg r_b;
+reg r_v;
 reg r_n;
 
 // C - Carry flag
@@ -80,6 +82,8 @@ begin
             r_c <= i_acr;
         else if (i_ir5_c)
             r_c <= i_ir5;
+        else if (i_db0_c)
+            r_c <= i_db[C];
     end
 end
 
@@ -92,7 +96,21 @@ begin
     begin
         if (i_dbz_z)
             r_z <= w_dbz;
+        else if (i_db1_z)
+            r_z <= i_db[Z];
     end
+end
+
+// D - decimal flag
+always @(negedge i_reset_n or negedge i_clk)
+begin
+    if (!i_reset_n)
+        r_d <= 0;
+    else
+    begin
+        if (i_db3_d)
+            r_d <= i_db[D];
+    end 
 end
 
 // I - Interrupt Flag
@@ -104,7 +122,33 @@ begin
     begin
         if (i_ir5_i)
             r_i <= i_ir5;
+        else if (i_db2_i)
+            r_i <= i_db[I];
     end 
+end
+
+// B - Break Command
+always @(negedge i_reset_n or negedge i_clk)
+begin
+    if (!i_reset_n)
+        r_b <= 0;
+    else
+    begin
+        if (i_db4_b)
+            r_b <= i_db[B];
+    end 
+end
+
+// V - Overflow Flag
+always @(negedge i_reset_n or negedge i_clk)
+begin
+    if (!i_reset_n)
+        r_v <= 0;
+    else
+    begin
+        if (i_db6_v)
+            r_v <= i_db[V]; 
+    end
 end
 
 // N - Negative Flag
@@ -123,9 +167,10 @@ end
 assign o_p[C] = r_c;
 assign o_p[Z] = r_z;
 assign o_p[I] = r_i;
-assign o_p[D] = 0;
-assign o_p[B] = 0;
-assign o_p[V] = 0;
+assign o_p[D] = r_d;
+assign o_p[B] = r_b;
+assign o_p[5] = 1'b0;
+assign o_p[V] = r_v;
 assign o_p[N] = r_n;
 
 endmodule
