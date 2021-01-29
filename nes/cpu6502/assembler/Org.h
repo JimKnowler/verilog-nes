1182ca945da26a6a1ca79a6e5268bec74f351223
Jim Knowler <jim.knowler2@gmail.com>
2021-01-29 11:48:09 +0000

    Org/Word: add documentation

--- nes/cpu6502/assembler/Org.h
+++ nes/cpu6502/assembler/Org.h
@@ -4,6 +4,13 @@
 
 namespace cpu6502 {
     namespace assembler {
+        /// @class Org
+        /// @brief Implement the .org assembler mneumonic for placing assembled code/data in memory
+        /// @param address 16bit address 
+        /// @note When .org is the first mneumonic (i.e. at byte index 0) then it is used by assembler to relocate 
+        ///       all addresses for a memory mapped ROM/RAM
+        /// @note When .org is used within other mneumonics (i.e. at byte index > 0) this it is used by the assembler
+        ///       to insert padding bytes so that the following byte code is positioned as requested
         class Org : public Opcode {
         public:
             Org(uint16_t address);
--- nes/cpu6502/assembler/Word.h
+++ nes/cpu6502/assembler/Word.h
@@ -4,6 +4,9 @@
 
 namespace cpu6502 {
     namespace assembler {
+        /// @class Word
+        /// @brief Assembler mneumonic for 16bits of raw data
+        /// @param address raw data is implemented as an address, so we support string labels or numeric byte index
         class Word : public Opcode {
         public:
             Word(const Address& address);
