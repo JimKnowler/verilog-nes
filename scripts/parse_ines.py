70a0705bdd4983d1f641c2b29a5367a9e42d9c32
Jim Knowler <jim.knowler2@gmail.com>
2021-02-22 10:25:35 +0000

    scripts: parse_ines.py - export CHR rom banks

--- scripts/parse_ines.py
+++ scripts/parse_ines.py
@@ -1,10 +1,16 @@
 # parse iNes format rom files
 
 import argparse
+from PIL import Image
+
+SIZE_INES_HEADER = 16
+SIZE_PRG_ROM_BANK = 16 * 1024
+SIZE_CHR_ROM_BANK = 8 * 1024
 
 def parse_args():
     parser = argparse.ArgumentParser(description='parse iNes format ROM')
     parser.add_argument('--file', '-f', required=True, dest='file', help='filepath of .nes ROM file to parse')
+    parser.add_argument('--chr', '-c', dest='export_chr_rom', action='store_true', help='export character ROM as PNG files' )
     args = parser.parse_args()
 
     return args
@@ -49,16 +55,57 @@ def parse_ines_header(header):
 
     return parsed_header
 
-def parse_ines(data):
-    SIZE_HEADER = 16
-    header = parse_ines_header(data[0:SIZE_HEADER])
-    
+def validate_ines(data, header):
     assert(not header['has_trainer'])
     size_prg_rom = header['size_prg_rom']
     size_chr_rom = header['size_chr_rom']
     
-    expected_file_size = SIZE_HEADER + (size_prg_rom * 16 * 1024) + (size_chr_rom * 8 * 1024)
+    expected_file_size = SIZE_INES_HEADER + (size_prg_rom * SIZE_PRG_ROM_BANK) + (size_chr_rom * SIZE_CHR_ROM_BANK)
     assert(expected_file_size == len(data))
+
+def export_ines_chr_rom_bank(index, chr_data):
+    BYTES_PER_TILE = 16         # 8 bits per row * 8 rows * 2 bits per pixels
+    NUM_TILES = int(len(chr_data) / BYTES_PER_TILE)
+
+    NUM_TILES_X = 32
+    NUM_TILES_Y = 16
+    assert(NUM_TILES == (NUM_TILES_X * NUM_TILES_Y))
+
+    image = Image.new("L", (8 * NUM_TILES_X, 8 * NUM_TILES_Y), 0)
+
+    tile_data_offset = 0
+    for tile_x in range(NUM_TILES_X):
+        for tile_y in range(NUM_TILES_Y):
+            tile_data = chr_data[tile_data_offset: tile_data_offset + BYTES_PER_TILE]
+            chr_x = tile_x * 8
+            chr_y = tile_y * 8
+            for x in range(8):
+                for y in range(8):
+                    bit0 = (tile_data[y] >> (7-x)) & 0x1
+                    bit1 = (tile_data[8+y] >> (7-x)) & 0x1
+
+                    pixel = (bit0 << 6) | (bit1 << 7)
+                    image.putpixel((chr_x + x,chr_y + y), pixel)
+
+            tile_data_offset += BYTES_PER_TILE
+    
+    filename = "chr_rom_bank_%d.png" % (index)
+    image.save(filename)
+
+def export_ines_chr_rom(data, header):
+    size_prg_rom = header['size_prg_rom']
+    offset_chr_rom = SIZE_INES_HEADER + (size_prg_rom * SIZE_PRG_ROM_BANK)
+    for i in range(header['size_chr_rom']):
+        bank_offset = offset_chr_rom + (i * SIZE_CHR_ROM_BANK)
+        export_ines_chr_rom_bank(i, data[bank_offset: bank_offset + SIZE_CHR_ROM_BANK])
+
+def parse_ines(data, export_chr_rom):
+    header = parse_ines_header(data[0:SIZE_INES_HEADER])
+    
+    validate_ines(data, header)
+
+    if export_chr_rom:
+        export_ines_chr_rom(data, header)
 
 def main():
     args = parse_args()
@@ -66,7 +113,7 @@ def main():
     with open(args.file, 'rb') as file:
         data = file.read()
 
-    parse_ines(data)
+    parse_ines(data, args.export_chr_rom)
 
 if __name__ == "__main__":
     main()
