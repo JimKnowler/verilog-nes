210fec3b7dac4bd947602410849f4fb0d85682db
Jim Knowler <jim.knowler2@gmail.com>
2021-05-19 09:08:31 +0100

    Cpu6502: test that NMI ignores I processor status flag

--- nes/cpu6502/test/Cpu6502.test.interrupt.cpp
+++ nes/cpu6502/test/Cpu6502.test.interrupt.cpp
@@ -422,12 +422,103 @@ TEST_F(Cpu6502, ShouldImplementNMI) {
     EXPECT_EQ(p | I, core.o_debug_p);
 }
 
+TEST_F(Cpu6502, ShouldNotIgnoreNMIWhenProcessorStatusHasI) {
+    auto& core = testBench.core();
+
+    sram.clear(0);
+
+    Assembler assembler;
+    assembler
+            .NOP()
+        .org(0x1234)
+        .label("init")
+            .SEC()                  // non-zero Processor Status (P)
+            .SED()
+            .SEI()                  // set the I flag for masking IRQ
+        .label("start")
+            .NOP()
+        .label("return_to")
+            .NOP()
+            .NOP()
+        .org(0x3456)
+        .label("non_maskable_interrupt")
+            .NOP()
+        .org(0xFFFA)                // INTERRUPT VECTOR
+        .word("non_maskable_interrupt")
+        .org(0xFFFC)                // RESET VECTOR
+        .word("init")
+        .compileTo(sram);
+
+    helperSkipResetVector();
+
+    // skip init section
+    testBench.tick(6);
+    testBench.trace.clear();
+
+    // capture information from the assembler / simulated core
+    cpu6502::assembler::Address start("start");
+    assembler.lookupAddress(start);
+
+    cpu6502::assembler::Address nonMaskableInterrupt("non_maskable_interrupt");
+    assembler.lookupAddress(nonMaskableInterrupt);
+
+    cpu6502::assembler::Address returnTo("return_to");
+    assembler.lookupAddress(returnTo);
+
+    const uint8_t sp = core.o_debug_s;
+    const uint8_t p = C | D | I;
+
+    // activate the NMI
+    core.i_nmi_n = 0;
+    testBench.tick(11);
+
+    Trace expected = TraceBuilder()
+        .port(i_clk).signal("_-").repeat(11)
+        .port(o_rw).signal("11110001111").repeatEachStep(2)
+        .port(o_sync).signal("10100000010").repeatEachStep(2)
+        .port(o_data)
+            .signal({0}).repeat(9)
+            .signal({returnTo.hi()}).repeat(2)
+            .signal({returnTo.lo()}).repeat(2)
+            .signal({p}).repeat(2)
+            .signal({0}).repeat(7)
+        .port(o_address)
+            .signal({
+                // NOP - at 'start'
+                start.byteIndex(),
+                start.byteIndex() + 1u,
+
+                // IRQ
+                returnTo.byteIndex(),
+                returnTo.byteIndex(),
+                0x0100u + sp,
+                0x0100u + sp - 1u,
+                0x0100u + sp - 2u,
+                0xFFFA,                     // Non Maskable Interrupt Vector (low byte)
+                0xFFFB,                     // Non Maskable Interrupt Vector (high byte)
+
+                // NOP (at interrupt vector)
+                nonMaskableInterrupt.byteIndex(),
+                nonMaskableInterrupt.byteIndex() + 1u
+            }).repeatEachStep(2)
+        .port(o_debug_s)
+            .signal({sp}).repeat(7)
+            .signal({sp - 3u}).repeat(4)
+            .concat().repeatEachStep(2);
+
+    EXPECT_THAT(testBench.trace, MatchesTrace(expected));
+
+    // processor flags should be augmented with I=Interrupt
+    EXPECT_EQ(p | I, core.o_debug_p);
+}
+
 // DONE: IRQ - should default simulation to 1
 // DONE: should handle IRQ - i_irq_n = 0, set I flag during interrupt
 // DONE: should ignore i_irq_n = 0, while I flag is set - then use it when the flag is cleared
-// should not ingore i_irq_n = 0, when returning from interrupt handler
 // DONE: NMI - should default simulation to 1
 // DONE: should handle NMI - i_nmi_n = 0 for 2 cycles after falling from 1 
-// should ignore i_nmi_n = 0 after handling NMI, until it goes high again
-// should not ignore i_nmi_n = 0, when I flag is set
+// DONE: should not ignore i_nmi_n = 0, when I flag is set
+
+// should not ingore i_irq_n = 0, when returning from interrupt handler
+// should ignore i_nmi_n = 0 after returning from NMI, until it goes high+low again
 // IRQ and NMI at the same time - prefer NMI, and then IRQ
