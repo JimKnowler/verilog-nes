4de0e041c3e117701ec5030b89b1d5bdcf27e557
Jim Knowler <jim.knowler2@gmail.com>
2021-05-23 17:55:20 +0100

    PPU: write to PPUCTRL

--- nes/ppu/PPU.v
+++ nes/ppu/PPU.v
@@ -1,11 +1,12 @@
 // PPU - Picture Processing Unit
-// AKA 2C02
+// AKA "2C02"
 
 module PPU(
-    /* verilator lint_off UNUSED */
-    /* verilator lint_off UNDRIVEN */
     input i_clk,
     input i_reset_n,
+
+    // chip select
+    input i_cs_n,
 
     // CPU interface
     output o_int_n,                 // ~Interrupt, to drive ~NMI on CPU
@@ -14,38 +15,110 @@ module PPU(
     output [7:0] o_data,            // Write to CPU data bus
     input i_rw,                     // Read/~Write for CPU data bus
 
-    // VIDEO interface    
-    output o_v_rd_n,                  // ~Read from VIDEO data bus
-    output o_v_we_n,                  // ~Write to VIDEO data bus
-    output [13:0] o_v_address,       // address for 
-    output [7:0] o_v_data,
-    input [7:0] i_v_data,
+    /* verilator lint_off UNUSED */
+    /* verilator lint_off UNDRIVEN */
 
-    // VGA output
-    output [7:0] o_vga_red,
-    output [7:0] o_vga_green,
-    output [7:0] o_vga_blue,
-    output o_vga_h_sync,
-    output o_vga_v_sync
+    // VIDEO interface    
+    output o_video_rd_n,                // ~Read from VIDEO data bus
+    output o_video_we_n,                // ~Write to VIDEO data bus
+    output [13:0] o_video_address,      // address for video data bus
+    output [7:0] o_video_data,          // data write to video data bus
+    input [7:0] i_video_data,           // data read from video data bus
+
+    // Video output
+    output [7:0] o_video_red,
+    output [7:0] o_video_green,
+    output [7:0] o_video_blue,
+    output o_video_x,
+    output o_video_y,
 
     /* verilator lint_on UNDRIVEN */
     /* verilator lint_on UNUSED */
 
+    // debug ports
+    output [7:0] o_debug_ppuctrl
 );
 
+localparam [2:0] RS_PPUCTRL = 0;
+localparam [2:0] RS_PPUSTATUS = 2;
+localparam RW_READ = 1;
+localparam RW_WRITE = 0;
+
 reg r_int_n;
-reg r_v_rd_n;
-reg r_v_we_n;
+reg r_video_rd_n;
+reg r_video_we_n;
+
+reg [7:0] r_data;
+reg [7:0] r_ppuctrl;
+reg [7:0] r_ppustatus;
+
+/* verilator lint_off UNUSED */
+
+// OAM sprite data
+reg [7:0] r_oam [255:0];
+
+// NMI_Occurred
+// - set true when vblank starts
+// - set false when vblank ends
+// - read through ppustatus[7], and set false after the read
+reg r_nmi_occurred;
+
+// NMI_Output
+// - PPU pulls o_nmi_n low when nmi_occurred && nmi_output
+// - set by writes to ppuctrl[7]
+reg r_nmi_output;
+
+/* verilator lint_on UNUSED */
 
 always @(*)
 begin
     r_int_n = 1;
-    r_v_rd_n = 1;
-    r_v_we_n = 1;
+    r_video_rd_n = 1;
+    r_video_we_n = 1;
+    
+    if (i_rw == RW_READ)
+    begin
+        case (i_rs)
+        RS_PPUSTATUS: begin
+            r_data = r_ppustatus;
+        end
+        default: begin
+            r_data = 0;
+        end
+        endcase
+    end
+    else
+    begin
+        r_data = 0;
+    end
+end
+
+always @(negedge i_reset_n or negedge i_clk)
+begin
+    if (i_reset_n == 0)
+    begin
+        r_ppuctrl <= 0;
+        r_ppustatus <= 0;
+    end
+    else if (i_cs_n == 0)
+    begin
+        if (i_rw == RW_WRITE)
+        begin
+            case (i_rs)
+            RS_PPUCTRL: begin
+                r_ppuctrl <= i_data;
+            end
+            default: begin
+            end
+            endcase
+        end
+    end
 end
 
 assign o_int_n = r_int_n;
-assign o_v_rd_n = r_v_rd_n;
-assign o_v_we_n = r_v_we_n;
+assign o_video_rd_n = r_video_rd_n;
+assign o_video_we_n = r_video_we_n;
+assign o_data = r_data;
+assign o_debug_ppuctrl = r_ppuctrl;
 
 endmodule
--- nes/ppu/test/PPU.test.cpp
+++ nes/ppu/test/PPU.test.cpp
@@ -7,6 +7,18 @@ using namespace gtestverilog;
 #include "nes/PPUTestBench.h"
 using namespace pputestbench;
 
+const int RS_PPUCTRL   = 0;
+const int RS_PPUMASK   = 1;
+const int RS_PPUSTATUS = 2;
+const int RS_OAMADDR   = 3;
+const int RS_OAMDATA   = 4;
+const int RS_PPUSCROLL = 5;
+const int RS_PPUADDR   = 6;
+const int RS_PPUDATA   = 7;
+
+const int RW_READ = 1;
+const int RW_WRITE = 0;
+
 namespace {
     class PPU : public ::testing::Test {
     public:
@@ -15,6 +27,9 @@ namespace {
 
             testBench.reset();
             testBench.trace.clear();
+
+            // enable chip select
+            testBench.core().i_cs_n = 0;
         }
         
         void TearDown() override {
@@ -32,6 +47,47 @@ TEST_F(PPU, ShouldInitialiseWithDefaultOutputs) {
     auto& core = testBench.core();
 
     EXPECT_EQ(1, core.o_int_n);
-    EXPECT_EQ(1, core.o_v_we_n);
-    EXPECT_EQ(1, core.o_v_rd_n);
+    EXPECT_EQ(1, core.o_video_we_n);
+    EXPECT_EQ(1, core.o_video_rd_n);
+
+    core.i_rs = RS_PPUSTATUS;
+    core.i_rw = RW_READ;
+    core.eval();
+    EXPECT_EQ(0, core.o_data);
 }
+
+TEST_F(PPU, ShouldWriteToPPUCTRL) {
+    auto& core = testBench.core();
+
+    core.i_rs = RS_PPUCTRL;
+    core.i_rw = RW_WRITE;
+    core.i_data = 0xFF;
+    core.eval();
+    EXPECT_EQ(0, core.o_debug_ppuctrl);
+    
+    testBench.tick(1);
+    EXPECT_EQ(0xFF, core.o_debug_ppuctrl);
+}
+
+// - TODO: o_nmi_n for vblank start/stop + repeats
+//         - when ppuctrl.v == 1 (not when 0)
+// - TODO: write ppumask - verify by reading from o_debug_ppumask
+// - TODO: read ppustatus
+//         - at startup - vblank clear
+//         - when nmi - vlank set + clear vblank
+// - TODO: write oamaddr - verify by reading from o_debug_oamaddr
+// - TODO: write oamdata - verify o_debug_oamaddr
+//          - auto increment oamaddr
+// - TODO: read oamdata
+//          - does NOT auto increment oamaddr
+// - TODO: write ppuscroll - verify by reading from o_debug_scroll_x, 
+//                           o_debug_scroll_y, w
+//         - 2 x writes (x, y)
+// - TODO: reset w - write ppuscroll x 1, read ppustatus
+// - TODO: write ppuaddr - verify by reading from o_debug_ppuaddr, w
+//         - 2 x writes (mxb, lsb)
+// - TODO: write ppudata
+//          - autoincrement ppuaddr
+// - TODO: read ppudata
+//          - autoincrement ppuaddr
+
