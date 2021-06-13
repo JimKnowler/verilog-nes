b978fe56b094e404b5de5c87db674d44964e336f
Jim Knowler <jim.knowler2@gmail.com>
2021-06-13 16:09:30 +0100

    PPUMemoryMap: basic implementation

--- nes/nes/PPUMemoryMap.v
+++ nes/nes/PPUMemoryMap.v
@@ -1,16 +1,64 @@
 module PPUMemoryMap(
+    /* verilator lint_off UNUSED */
+    /* verilator lint_off UNDRIVEN */
     input i_clk,
-    input i_reset_n
+    input i_reset_n,
+
+    /* verilator lint_on UNUSED */
+    /* verilator lint_on UNDRIVEN */
+
+    input [15:0] i_address_ppu,     // address that PPU is accessing
+    input i_rd_en_ppu_n,            // set to 0 when PPU is reading
+    input i_wr_en_ppu_n,            // set to 0 when PPU is writing
+    output [7:0] o_data_ppu,        // data output to PPU
+
+    output o_cs_patterntable,       // 1 when patterntable is selected
+    input [7:0] i_data_patterntable,// data read from patterntable
+    output o_rw_patterntable,       // 1 = READ, 0 = WRITE   
+    output [15:0] o_address_patterntable,   // address output to patterntable
+
+    output o_cs_nametable,          // 1 when nametable is selected
+    input [7:0] i_data_nametable,   // data read from nametable
+    output o_rw_nametable,          // 1 = READ, 0 = WRITE
+    output [15:0] o_address_nametable       // address output to nametable
 );
 
-always @(negedge i_reset_n or negedge i_clk)
+reg address_range;
+reg [7:0] r_data;
+reg [15:0] r_address;
+
+localparam ADDRESS_RANGE_PATTERNTABLE = 0;
+localparam ADDRESS_RANGE_NAMETABLE = 1;
+
+localparam RW_READ = 1;
+localparam RW_WRITE = 0;
+
+localparam ADDRESS_NAMETABLE_START = 16'h2000;
+
+always @(*)
 begin
-    if (!i_reset_n)
+    if (i_address_ppu < ADDRESS_NAMETABLE_START)
     begin
+        address_range = ADDRESS_RANGE_PATTERNTABLE;
+        r_data = i_data_patterntable;
+        r_address = i_address_ppu;
     end
     else
     begin
+        address_range = ADDRESS_RANGE_NAMETABLE;
+        r_data = i_data_nametable;
+        r_address = i_address_ppu & 16'h2FFF;
     end
 end
+
+wire w_ppu_wr_or_rd_en = (i_rd_en_ppu_n == 0) || (i_wr_en_ppu_n == 0);
+
+assign o_cs_patterntable = (address_range == ADDRESS_RANGE_PATTERNTABLE) && w_ppu_wr_or_rd_en;
+assign o_cs_nametable = (address_range == ADDRESS_RANGE_NAMETABLE) && w_ppu_wr_or_rd_en;
+assign o_data_ppu = r_data;
+assign o_address_patterntable = r_address;
+assign o_rw_patterntable = (i_rd_en_ppu_n == 0) ? RW_READ : RW_WRITE;
+assign o_address_nametable = r_address;
+assign o_rw_nametable = (i_rd_en_ppu_n == 0) ? RW_READ : RW_WRITE;
 
 endmodule
--- nes/nes/test/PPUMemoryMap.test.cpp
+++ nes/nes/test/PPUMemoryMap.test.cpp
@@ -9,6 +9,17 @@ using namespace gtestverilog;
 #include "nes/PPUMemoryMapTestBench.h"
 using namespace ppumemorymaptestbench;
 
+const int RW_READ = 1;
+const int RW_WRITE = 0;
+
+const int ADDRESS_PATTERNTABLE_START = 0x0000;
+const int ADDRESS_PATTERNTABLE_END = 0x1FFF;
+
+const int ADDRESS_NAMETABLE_START = 0x2000;
+const int ADDRESS_NAMETABLE_END = 0x2FFF;
+
+const int ADDRESS_NAMETABLE_MIRROR_START = 0x3000;
+const int ADDRESS_NAMETABLE_MIRROR_END = 0x3FFF;
 
 namespace {
     class PPUMemoryMap : public ::testing::Test {
@@ -26,17 +37,116 @@ TEST_F(PPUMemoryMap, ShouldConstruct) {
     // testing that test fixture can safely construct and reset
 }
 
-// mirror palette 0x3000/0x3FFF to 0x2000/0x2FFF
-// mirror nametables (0x2000/0x2FFF) based on h/v mirroring 
-//   -> including mirror of palette
+TEST_F(PPUMemoryMap, ShouldReadFromPatternTables) {
+    auto& core = testBench.core();
 
-// input i_nametable_mirroring : 0 = horizontal, 1 = vertical
-// memory port - CHR ROM 0x000 -> 0x1FFF
-//   - address, chip select, data (read)
-// memory port - nametable 0x2000 -> 0x2FFF
-//   - address, chip select, RW, data (read), data (write)
+    for (int address=ADDRESS_PATTERNTABLE_START, i=0; address<=ADDRESS_PATTERNTABLE_END; address++, i++) {
+        int data = i % 256;
+        core.i_address_ppu = address;
+        core.i_rd_en_ppu_n = 0;
+        core.i_wr_en_ppu_n = 1;
+        core.i_data_patterntable = data;
+        core.eval();
 
-// NAMETABLE VRAM
-// output - chip select
-// output read/write
-// input/output data
+        EXPECT_EQ(RW_READ, core.o_rw_patterntable);
+        EXPECT_EQ(address, core.o_address_patterntable);
+
+        EXPECT_EQ(1, core.o_cs_patterntable);
+        EXPECT_EQ(0, core.o_cs_nametable);
+        EXPECT_EQ(data, core.o_data_ppu);
+    }
+}
+
+TEST_F(PPUMemoryMap, ShouldReadFromNametable) {
+    auto& core = testBench.core();
+
+    for (int address=ADDRESS_NAMETABLE_START, i=0; address<=ADDRESS_NAMETABLE_END; address++, i++) {
+        int data = i % 256;
+        core.i_address_ppu = address;
+        core.i_rd_en_ppu_n = 0;
+        core.i_wr_en_ppu_n = 1;
+        core.i_data_nametable = data;
+        core.eval();
+
+        EXPECT_EQ(RW_READ, core.o_rw_nametable);
+        EXPECT_EQ(address, core.o_address_nametable);
+
+        EXPECT_EQ(0, core.o_cs_patterntable);
+        EXPECT_EQ(1, core.o_cs_nametable);
+        EXPECT_EQ(data, core.o_data_ppu);
+    }
+}
+
+TEST_F(PPUMemoryMap, ShouldWriteToNametable) {
+    auto& core = testBench.core();
+
+    for (int address=ADDRESS_NAMETABLE_START, i=0; address<=ADDRESS_NAMETABLE_END; address++, i++) {
+        int data = i % 256;
+        core.i_address_ppu = address;
+        core.i_rd_en_ppu_n = 1;
+        core.i_wr_en_ppu_n = 0;
+        core.i_data_nametable = data;
+        core.eval();
+
+        EXPECT_EQ(RW_WRITE, core.o_rw_nametable);
+        EXPECT_EQ(address, core.o_address_nametable);
+
+        EXPECT_EQ(0, core.o_cs_patterntable);
+        EXPECT_EQ(1, core.o_cs_nametable);
+        EXPECT_EQ(data, core.o_data_ppu);
+    }
+}
+
+TEST_F(PPUMemoryMap, ShouldReadFromNametableMirror) {
+    auto& core = testBench.core();
+
+    for (int address=ADDRESS_NAMETABLE_MIRROR_START, i=0; address<=ADDRESS_NAMETABLE_MIRROR_END; address++, i++) {
+        int data = i % 256;
+        core.i_address_ppu = address;
+        core.i_rd_en_ppu_n = 0;
+        core.i_wr_en_ppu_n = 1;
+        core.i_data_nametable = data;
+        core.eval();
+
+        EXPECT_EQ(RW_READ, core.o_rw_nametable);
+        EXPECT_EQ(address - 0x1000, core.o_address_nametable);
+
+        EXPECT_EQ(0, core.o_cs_patterntable);
+        EXPECT_EQ(1, core.o_cs_nametable);
+        EXPECT_EQ(data, core.o_data_ppu);
+    }
+}
+
+TEST_F(PPUMemoryMap, ShouldWriteToNametableMirror) {
+    auto& core = testBench.core();
+
+    for (int address=ADDRESS_NAMETABLE_MIRROR_START, i=0; address<=ADDRESS_NAMETABLE_MIRROR_END; address++, i++) {
+        int data = i % 256;
+        core.i_address_ppu = address;
+        core.i_rd_en_ppu_n = 1;
+        core.i_wr_en_ppu_n = 0;
+        core.i_data_nametable = data;
+        core.eval();
+
+        EXPECT_EQ(RW_WRITE, core.o_rw_nametable);
+        EXPECT_EQ(address - 0x1000, core.o_address_nametable);
+
+        EXPECT_EQ(0, core.o_cs_patterntable);
+        EXPECT_EQ(1, core.o_cs_nametable);
+        EXPECT_EQ(data, core.o_data_ppu);
+    }
+}
+
+TEST_F(PPUMemoryMap, ShouldNotEnableChipSelectOutputWhenPPUIsNotReadingOrWriting) {
+    auto& core = testBench.core();
+
+    for (int address=ADDRESS_PATTERNTABLE_START, i=0; address<=ADDRESS_NAMETABLE_MIRROR_END; address++, i++) {
+        core.i_address_ppu = address;
+        core.i_rd_en_ppu_n = 1;
+        core.i_wr_en_ppu_n = 1;
+        core.eval();
+
+        EXPECT_EQ(0, core.o_cs_patterntable);
+        EXPECT_EQ(0, core.o_cs_nametable);
+    }
+}
