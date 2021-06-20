19885f2b72b0548b135f7ca22320ac00a3195242
Jim Knowler <jim.knowler2@gmail.com>
2021-06-20 16:27:17 +0100

    Cpu6502.test: test RESET while IRQ / NMI are active low

--- nes/cpu6502/test/Cpu6502.test.cpp
+++ nes/cpu6502/test/Cpu6502.test.cpp
@@ -43,6 +43,68 @@ TEST_F(Cpu6502, ShouldImplementResetVector) {
     EXPECT_THAT(testBench.trace, MatchesTrace(expected));
 }
 
+TEST_F(Cpu6502, ShouldImplementResetVectorWhileIRQ) {
+    sram.clear(NOP());
+
+    testBench.core().i_irq_n = 0;
+
+    // write address for reset vector
+    sram.write(0xFFFC, 0x12);               // low byte of 16bit address
+    sram.write(0xFFFD, 0x80);               // high byte of 16bit address
+
+    testBench.tick(9);
+
+    Trace expected = TraceBuilder()
+        .port(i_clk).signal("_-").repeat(9)
+        .port(o_rw).signal("11").repeat(9)
+        .port(o_sync).signal("100000010").repeatEachStep(2)
+        .port(o_address)
+            .signal({
+                0x0000, 0x0000,             // PC, PC
+                0x01FF, 0x01FE, 0x01FD,     // SP, SP-1, SP-2
+                0xFFFC, 0xFFFD,             // Reset Vector (low byte), Reset Vector (high byte)
+                0x8012,                     // The reset vector (pointing at NOP)
+                0x8013,                     // NOP incrememting PC
+            }).repeatEachStep(2)
+        .port(o_debug_s)
+            .signal({0xFF}).repeat(5)
+            .signal({0xFC}).repeat(4)       // store SP-3 after 3rd push
+            .concat().repeatEachStep(2);
+
+    EXPECT_THAT(testBench.trace, MatchesTrace(expected));
+}
+
+TEST_F(Cpu6502, ShouldImplementResetVectorWhileNMI) {
+    sram.clear(NOP());
+
+    // write address for reset vector
+    sram.write(0xFFFC, 0x12);               // low byte of 16bit address
+    sram.write(0xFFFD, 0x80);               // high byte of 16bit address
+
+    testBench.tick(1);
+    testBench.core().i_nmi_n = 0;
+    testBench.tick(8);
+
+    Trace expected = TraceBuilder()
+        .port(i_clk).signal("_-").repeat(9)
+        .port(o_rw).signal("11").repeat(9)
+        .port(o_sync).signal("100000010").repeatEachStep(2)
+        .port(o_address)
+            .signal({
+                0x0000, 0x0000,             // PC, PC
+                0x01FF, 0x01FE, 0x01FD,     // SP, SP-1, SP-2
+                0xFFFC, 0xFFFD,             // Reset Vector (low byte), Reset Vector (high byte)
+                0x8012,                     // The reset vector (pointing at NOP)
+                0x8013,                     // NOP incrememting PC
+            }).repeatEachStep(2)
+        .port(o_debug_s)
+            .signal({0xFF}).repeat(5)
+            .signal({0xFC}).repeat(4)       // store SP-3 after 3rd push
+            .concat().repeatEachStep(2);
+
+    EXPECT_THAT(testBench.trace, MatchesTrace(expected));
+}
+
 TEST_F(Cpu6502, ShouldImplementNOP) {
     sram.clear(NOP());
 
