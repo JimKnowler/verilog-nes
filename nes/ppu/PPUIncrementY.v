bccbb210dd980a1af82cc182b18917259ca37012
Jim Knowler <jim.knowler2@gmail.com>
2021-07-10 18:44:52 +0100

    PPUIncrementY: implement module

--- nes/ppu/PPUIncrementY.v
+++ nes/ppu/PPUIncrementY.v
@@ -1,26 +1,42 @@
 module PPUIncrementY(
     /* verilator lint_off UNUSED */
     input i_clk,
-    input i_reset_n
+    input i_reset_n,
     /* verilator lint_on UNUSED */
+
+    input [14:0] i_v,
+    output [14:0] o_v
 );
 
 // https://wiki.nesdev.com/w/index.php/PPU_scrolling#Y_increment
 
-/*
-if ((v & 0x7000) != 0x7000)        // if fine Y < 7
-  v += 0x1000                      // increment fine Y
-else
-  v &= ~0x7000                     // fine Y = 0
-  int y = (v & 0x03E0) >> 5        // let y = coarse Y
-  if (y == 29)
-    y = 0                          // coarse Y = 0
-    v ^= 0x0800                    // switch vertical nametable
-  else if (y == 31)
-    y = 0                          // coarse Y = 0, nametable not switched
+reg [14:0] r_v;
+
+always @(*)
+begin
+  if ((i_v & 15'h7000) != 15'h7000)
+  begin
+    // fine
+    r_v = i_v + 15'h1000;
+  end
   else
-    y += 1                         // increment coarse Y
-  v = (v & ~0x03E0) | (y << 5)     // put coarse Y back into v
-  */
+  begin
+    // coarse
+    r_v = i_v & ~15'h7000;      // fine y = 0
+
+    if (i_v[9:5] == 29)
+    begin
+      // overflow
+      r_v[9:5] = 0;
+      r_v[11] = ~r_v[11];
+    end
+    else
+    begin
+      r_v[9:5] = r_v[9:5] + 1;
+    end
+  end
+end
+
+assign o_v = r_v;
 
 endmodule
--- nes/ppu/test/PPUIncrementY.test.cpp
+++ nes/ppu/test/PPUIncrementY.test.cpp
@@ -8,6 +8,10 @@ using namespace gtestverilog;
 using namespace ppuincrementytestbench;
 
 namespace {
+    uint32_t BIT_OFFSET_FINE_Y = 12;
+    uint32_t BIT_OFFSET_COARSE_Y = 5;
+    uint32_t BIT_OFFSET_VERTICAL_NAMETABLE = 11;
+
     class PPUIncrementY : public ::testing::TestWithParam<int> {
     public:
         void SetUp() override {
@@ -20,6 +24,42 @@ namespace {
     };
 }
 
-TEST_F(PPUIncrementY, ShouldConstruct) {
-    // empty
+TEST_F(PPUIncrementY, ShouldIncrementFine) {
+    auto& core = testBench.core();
+
+    for (uint16_t y=0; y<7; y++) {
+        core.i_v = y << BIT_OFFSET_FINE_Y;
+        core.eval();
+
+        EXPECT_EQ(core.o_v, (y+1) << 12);
+    }
 }
+
+TEST_F(PPUIncrementY, ShouldIncrementCoarse) {
+    auto& core = testBench.core();
+
+    core.i_v = 7 << BIT_OFFSET_FINE_Y;
+    core.eval();
+
+    EXPECT_EQ(core.o_v, 1 << BIT_OFFSET_COARSE_Y);
+}
+
+TEST_F(PPUIncrementY, ShouldIncrementCoarseOverflowtoSecondVerticalNametable) {
+    auto& core = testBench.core();
+
+    core.i_v = (7 << BIT_OFFSET_FINE_Y) | (29 << BIT_OFFSET_COARSE_Y);
+    core.eval();
+
+    EXPECT_EQ(core.o_v, 1 << BIT_OFFSET_VERTICAL_NAMETABLE);
+}
+
+TEST_F(PPUIncrementY, ShouldIncrementCoarseOverflowBackToFirstVerticalNametable) {
+    auto& core = testBench.core();
+
+    core.i_v = (7 << BIT_OFFSET_FINE_Y) | (29 << BIT_OFFSET_COARSE_Y) | (1 << BIT_OFFSET_VERTICAL_NAMETABLE);
+    core.eval();
+
+    EXPECT_EQ(core.o_v, 0);
+}
+
+// TODO: if y == 31, set y  = 0 without toggling vertical nametable
