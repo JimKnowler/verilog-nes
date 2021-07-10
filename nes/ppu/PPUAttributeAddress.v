550ec49632d4b6b73c0de741ae99062574a251b1
Jim Knowler <jim.knowler2@gmail.com>
2021-07-10 18:55:22 +0100

    PPUAttributeAddress: implement module

--- nes/ppu/PPUAttributeAddress.v
+++ nes/ppu/PPUAttributeAddress.v
@@ -1,10 +1,22 @@
 module PPUAttributeAddress(
     /* verilator lint_off UNUSED */
     input i_clk,
-    input i_reset_n
+    input i_reset_n,
     /* verilator lint_on UNUSED */
+
+    input [14:0] i_v,
+    output [14:0] o_address
 );
 
 // https://wiki.nesdev.com/w/index.php/PPU_scrolling#Tile_and_attribute_fetching
+
+reg [14:0] r_v;
+
+always @(*)
+begin
+    r_v = 15'h23C0 | (i_v & 15'h0C00) | { {9{1'b0}}, i_v[9:7], {3{1'b0}}} | { {12{1'b0}}, i_v[4:2] };
+end
+
+assign o_address = r_v;
 
 endmodule
--- nes/ppu/test/PPUAttributeAddress.test.cpp
+++ nes/ppu/test/PPUAttributeAddress.test.cpp
@@ -20,6 +20,13 @@ namespace {
     };
 }
 
-TEST_F(PPUAttributeAddress, ShouldConstruct) {
-    // empty
+TEST_F(PPUAttributeAddress, ShouldOutputCorrectAddress) {
+    auto& core = testBench.core();
+
+    for (uint16_t v=0; v < (1<<15); v++) {
+        core.i_v = v;
+        core.eval();
+
+        ASSERT_EQ(core.o_address, 0x23C0 | (v & 0x0C00) | ((v >> 4) & 0x38) | ((v >> 2) & 0x07));
+    }
 }
