dbf121a3a57b57f13f14bcaaf1e88767bc271fbc
Jim Knowler <jim.knowler2@gmail.com>
2021-07-24 16:02:54 +0100

    Shift8: add 8 bit shift register for attribute table

---
+++ nes/ppu/Shift8.v
@@ -0,0 +1,53 @@
+// 8 bit shift register
+// - serial shift in
+// - parallel out
+
+module Shift8(
+    input i_clk,
+    input i_reset_n,
+
+    input i_load,               // control - load value from i_data into most significant bit
+    input i_data,               // input data - value to load into most-significant bit
+
+    input i_shift,              // control - left shift on next clock pulse
+    input [2:0] i_offset,       // offset into top 8 bits of shift register
+    output o_shift_data,        // output data - value of offset bit in the shift register
+
+    output [7:0] o_debug_data   // debug - current value in the shift register
+
+);
+
+reg [7:0] r_data;
+
+always @(negedge i_reset_n or posedge i_clk)
+begin
+    if (!i_reset_n)
+    begin
+        r_data <= 0;
+    end
+    else
+    begin
+        if (i_shift)
+        begin
+            r_data[6:0] <= r_data[7:1];
+
+            if (i_load)
+            begin
+                r_data[7] <= i_data;
+            end
+            else
+            begin    
+                r_data[0] <= 0;
+            end
+        end
+        else if (i_load)
+        begin
+            r_data[7] <= i_data;
+        end
+    end
+end
+
+assign o_debug_data = r_data;
+assign o_shift_data = r_data[i_offset];
+
+endmodule
---
+++ nes/ppu/test/Shift8.test.cpp
@@ -0,0 +1,99 @@
+#include <gtest/gtest.h>
+using namespace testing;
+
+#include "gtestverilog/gtestverilog.h"
+using namespace gtestverilog;
+
+#include "nes/Shift8TestBench.h"
+using namespace shift8testbench;
+
+namespace {
+    class Shift8 : public ::testing::Test {
+    public:
+        void SetUp() override {
+            testBench.setClockPolarity(1);
+
+            testBench.reset();
+            testBench.trace.clear();
+        }
+        
+        void TearDown() override {
+        }
+
+        void helperLoadValue(uint8_t value) {
+            auto& core = testBench.core();
+            core.i_shift = 1;
+            core.i_load = 1;
+
+            for (int i=0; i<8; i++) {
+                core.i_data = (value >> i) & 1;
+                testBench.tick();
+            }
+        }
+
+        Shift8TestBench testBench;
+    };
+}
+
+TEST_F(Shift8, ShouldConstruct) {
+    auto& core = testBench.core();
+    ASSERT_EQ(0, core.o_debug_data);
+}
+
+TEST_F(Shift8, ShouldShiftWithLoad) {
+    auto& core = testBench.core();
+    core.i_shift = 1;
+    core.i_load = 1;
+    core.i_data = 1;
+    testBench.tick();
+
+    ASSERT_EQ(1 << 7, core.o_debug_data);
+}
+
+TEST_F(Shift8, ShouldShiftWithoutLoad) {
+    auto& core = testBench.core();
+    core.i_shift = 1;
+    core.i_load = 0;
+    core.i_data = 1;
+    testBench.tick();
+
+    ASSERT_EQ(0, core.o_debug_data);
+}
+
+TEST_F(Shift8, ShouldShiftRepeatedly) {
+    auto& core = testBench.core();
+
+    const uint8_t kTestValue = 0x3a;
+    helperLoadValue(kTestValue);
+    
+    ASSERT_EQ(kTestValue, core.o_debug_data);
+}
+
+TEST_F(Shift8, ShouldLoadWithoutShift) {
+    auto& core = testBench.core();
+
+    const uint8_t kTestValue = 0b01100110;
+    helperLoadValue(kTestValue);
+
+    core.i_load = 1;
+    core.i_data = 0;
+    core.i_shift = 0;
+    testBench.tick();
+
+    ASSERT_EQ(kTestValue & ~0x80, core.o_debug_data);
+}
+
+TEST_F(Shift8, ShouldReadOffsetBit) {
+    auto& core = testBench.core();
+
+    const uint8_t kTestValue = 0b10010010;
+    helperLoadValue(kTestValue);
+
+    for (int i=0; i<8; i++) {
+        core.i_offset = i;
+        core.eval();
+        EXPECT_EQ((kTestValue >> i) & 0x01, core.o_shift_data);        
+    }
+}
+
+// todo: posedge or negedge?
