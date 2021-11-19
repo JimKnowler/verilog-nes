fe2e46737235dc3ac80244ff80901835614a1f81
Jim Knowler <jim.knowler2@gmail.com>
2021-11-19 18:46:33 +0000

    DebuggerTop: integrate 6502CPU + SYNC

--- nes/debugger/DebuggerTop.v
+++ nes/debugger/DebuggerTop.v
@@ -4,7 +4,7 @@
 
 module DebuggerTop(
     input   i_clk_100mhz,
-    //input   i_clk_5mhz,
+    input   i_clk_5mhz,
     input   i_reset_n,
     
     // SPI interface
@@ -118,13 +118,137 @@ Memory memory (
   .o_data(w_mem_data_rd)
 );
 
+//
+// Synchronisation between 100mhz and 5mhz clocks
+//
+
+wire w_start_step_100mhz;
+wire w_start_step_5mhz;
+
+Sync syncFrom100Mhz(
+    .i_clk(i_clk_100mhz),
+    .i_reset_n(i_reset_n),
+    .i_data(w_start_step_100mhz),
+    
+    .i_sync_clk(i_clk_5mhz),
+    .o_sync_posedge(w_start_step_5mhz)
+);
+
+reg r_step_completed_5mhz;
+wire w_step_completed_100mhz;
+
+Sync syncTo100Mhz(
+    .i_clk(i_clk_5mhz),
+    .i_reset_n(i_reset_n),
+    .i_data(r_step_completed_5mhz),
+    
+    .i_sync_clk(i_clk_100mhz),
+    .o_sync_posedge(w_step_completed_100mhz)
+);
+
+//
+// 6502 CPU
+//
+
+/* verilator lint_off UNUSED */
+reg r_clk_en_cpu;
+wire w_rw_cpu;
+wire [15:0] w_address_cpu;
+reg [7:0] r_i_data_cpu;
+wire [7:0] w_o_data_cpu;
+reg r_irq_n;
+reg r_nmi_n;
+wire w_sync_cpu;
+wire [7:0] w_cpu_reg_a;
+wire [7:0] w_cpu_reg_x;
+wire [7:0] w_cpu_reg_y;
+wire [7:0] w_cpu_reg_s;
+wire [7:0] w_cpu_reg_p;
+wire [7:0] w_cpu_reg_ir;
+/* verilator lint_on UNUSED */
+
+always @(posedge i_clk_5mhz or negedge i_reset_n)
+begin
+    if (!i_reset_n)
+    begin
+        r_clk_en_cpu <= 0;
+        r_i_data_cpu <= 0;
+        r_irq_n <= 1;
+        r_nmi_n <= 1;
+    end
+    else
+    begin
+        if (w_start_step_5mhz)
+        begin
+            r_clk_en_cpu <= 1;
+        end
+        else
+        begin
+            r_clk_en_cpu <= 0;
+
+            if (r_clk_en_cpu == 1)
+            begin
+                r_step_completed_5mhz <= 1;
+            end
+            else
+            begin
+                r_step_completed_5mhz <= 0;
+            end
+        end
+    end
+end
+
+/* verilator lint_off PINMISSING */
+Cpu6502 cpu6502(
+    .i_clk(i_clk_5mhz),
+    .i_reset_n(i_reset_n),
+    .i_clk_en(r_clk_en_cpu),
+    .o_rw(w_rw_cpu),
+    .o_address(w_address_cpu),
+    .i_data(r_i_data_cpu),
+    .o_data(w_o_data_cpu),
+    .i_irq_n(r_irq_n),
+    .i_nmi_n(r_nmi_n),
+    .o_sync(w_sync_cpu),
+
+    .o_debug_ac(w_cpu_reg_a),
+    .o_debug_x(w_cpu_reg_x),
+    .o_debug_y(w_cpu_reg_y),
+    .o_debug_s(w_cpu_reg_s),
+    .o_debug_p(w_cpu_reg_p),
+    .o_debug_ir(w_cpu_reg_ir)
+);
+/* verilator lint_on PINMISSING */
+
+//
+// Values - control CPU step + read CPU values
+//
+
 Values values (
     .i_clk(i_clk_100mhz),
+    .i_reset_n(i_reset_n),
+
     .i_ena(w_value_en),
     .i_wea(r_is_value_wen),
     .i_id(w_value_id),
     .i_data(w_value_data_wr),
-    .o_data(w_value_data_rd)
+    .o_data(w_value_data_rd),
+
+    .i_cpu_address(w_address_cpu),
+    .i_cpu_data(w_o_data_cpu),
+    .i_cpu_rw(w_rw_cpu),
+    .i_cpu_irq_n(r_irq_n),
+    .i_cpu_nmi_n(r_nmi_n),
+    .i_cpu_sync(w_sync_cpu),
+    .i_cpu_reg_a(w_cpu_reg_a),
+    .i_cpu_reg_x(w_cpu_reg_x),
+    .i_cpu_reg_y(w_cpu_reg_y),
+    .i_cpu_reg_s(w_cpu_reg_s),
+    .i_cpu_reg_p(w_cpu_reg_p),
+    .i_cpu_reg_ir(w_cpu_reg_ir),
+    
+    .o_cpu_start_step(w_start_step_100mhz),
+    .i_cpu_step_completed(w_step_completed_100mhz)
 );
 
 endmodule
---
+++ nes/debugger/Values.v
@@ -0,0 +1,136 @@
+/*
+ * Write: VALUDID_CPU_START_STEP = 1, to trigger a single step of CPU
+ * Read: VALUEID_CPU_START_STEP, =1 while stepping, =0 when finished stepping
+ * Read: VALUDID_CPU_xxxx to read state of CPU
+ */
+
+module Values(
+    input i_clk,
+    input i_reset_n,
+
+    input i_ena,
+    input i_wea,
+    input [15:0] i_id,
+    input [15:0] i_data,
+    output [15:0] o_data,
+
+    // CPU fields
+    input [15:0] i_cpu_address,
+    input [7:0] i_cpu_data,
+    input i_cpu_rw,
+    input i_cpu_irq_n,
+    input i_cpu_nmi_n,
+    input i_cpu_sync,
+    input [7:0] i_cpu_reg_a,
+    input [7:0] i_cpu_reg_x,
+    input [7:0] i_cpu_reg_y,
+    input [7:0] i_cpu_reg_s,
+    input [7:0] i_cpu_reg_p,
+    input [7:0] i_cpu_reg_ir,
+    
+
+    // CPU step control signals
+    output o_cpu_start_step,
+    input i_cpu_step_completed
+);
+
+// start a CPU step
+localparam VALUEID_CPU_START_STEP = 1;
+
+// Retrieve CPU values
+localparam VALUEID_CPU_ADDRESS = 2;
+localparam VALUEID_CPU_DATA = 3;
+localparam VALUEID_CPU_RW = 4;
+localparam VALUEID_CPU_IRQ_N = 5;
+localparam VALUEID_CPU_NMI_N = 6;
+localparam VALUEID_CPU_SYNC = 7;
+localparam VALUEID_CPU_REG_A = 8;
+localparam VALUEID_CPU_REG_X = 8;
+localparam VALUEID_CPU_REG_Y = 8;
+localparam VALUEID_CPU_REG_S = 8;
+localparam VALUEID_CPU_REG_P = 8;
+localparam VALUEID_CPU_REG_IR = 8;
+
+localparam NUM_VALUES = 16;
+
+reg r_cpu_start_step;
+
+reg [15:0] r_value;
+
+always @(posedge i_clk or negedge i_reset_n)
+begin
+    if (!i_reset_n)
+    begin
+        r_cpu_start_step <= 0;
+    end
+    else
+    begin
+        if (i_cpu_step_completed == 1)
+        begin
+            r_cpu_start_step <= 0;
+        end
+                
+        if (i_ena)
+        begin
+            if (i_wea)
+            begin
+                if (i_id == VALUEID_CPU_START_STEP)
+                begin
+                    r_cpu_start_step <= (i_data == 1);
+                end
+            end
+        end
+    end
+end
+
+always @(*)
+begin
+    case (i_id)
+    VALUEID_CPU_START_STEP: begin
+        r_value = { 15'd0, r_cpu_start_step };
+    end
+    VALUEID_CPU_ADDRESS: begin
+        r_value = i_cpu_address;
+    end
+    VALUEID_CPU_DATA: begin
+        r_value = { 8'd0, i_cpu_data };
+    end
+    VALUEID_CPU_RW: begin
+        r_value = { 15'd0, i_cpu_rw };
+    end
+    VALUEID_CPU_IRQ_N: begin
+        r_value = { 15'd0, i_cpu_irq_n };
+    end
+    VALUEID_CPU_NMI_N: begin
+        r_value = { 15'd0, i_cpu_nmi_n };
+    end
+    VALUEID_CPU_SYNC: begin
+        r_value = { 15'd0, i_cpu_sync };
+    end
+    VALUEID_CPU_REG_A: begin
+        r_value = { 8'd0, i_cpu_reg_a };
+    end
+    VALUEID_CPU_REG_X: begin
+        r_value = { 8'd0, i_cpu_reg_x };
+    end
+    VALUEID_CPU_REG_Y: begin
+        r_value = { 8'd0, i_cpu_reg_y };
+    end
+    VALUEID_CPU_REG_S: begin
+        r_value = { 8'd0, i_cpu_reg_s };
+    end
+    VALUEID_CPU_REG_P: begin
+        r_value = { 8'd0, i_cpu_reg_p };
+    end
+    VALUEID_CPU_REG_IR: begin
+        r_value = { 8'd0, i_cpu_reg_ir };
+    end
+    default:
+        r_value = 0;
+    endcase
+end
+
+assign o_data = i_ena ? r_value : 0;
+assign o_cpu_start_step = r_cpu_start_step;
+
+endmodule
