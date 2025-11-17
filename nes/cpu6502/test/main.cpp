86c87e078ba93cf237a0d86efa07ee3a50e98839
Jim Knowler <jim.knowler2@gmail.com>
2025-11-17 20:21:39 +0000

    cpu6502/test: main.cpp - temporarily add 'sc_time_stamp' to get project to link

--- nes/cpu6502/test/main.cpp
+++ nes/cpu6502/test/main.cpp
@@ -2,6 +2,12 @@
 #include <verilated.h>
 #include <gmock/gmock.h>
 
+// required for linking with Verilator
+double sc_time_stamp()
+{ 
+    return 0; 
+}
+
 int main(int argc, char **argv){
     // initialise Verilator
     Verilated::commandArgs(argc, argv);
